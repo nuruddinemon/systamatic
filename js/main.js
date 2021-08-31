1ae1536be50c139917ae022ff2337cb666670311
N-U-Emon <76093791+N-U-Emon@users.noreply.github.com>
2021-08-31 20:49:16 +0600

    add files

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//bx slider
+
+$(document).ready(function(){
+    $('.slider').bxSlider();
+  });
+//meanmenu
+$(document).ready(function () {
+	$('#main_menu').meanmenu({
+    menaMenuContainer : '#mobile_menu',
+    meanScreenWidth : 991,
+  });
+});
